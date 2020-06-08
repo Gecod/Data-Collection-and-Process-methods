7f67d08c51a6e600eab44bf61280463de65f2b6b
Gecod <56275019+Gecod@users.noreply.github.com>
2020-06-08 23:23:45 +0400

    Add files via upload

--- 02/Lesson_02-1.py
+++ 02/Lesson_02-1.py
@@ -17,61 +17,35 @@
 from bs4 import BeautifulSoup as bs
 import requests
 from pprint import pprint
-from time import sleep
-import json
 
-# https://samara.hh.ru/search/vacancy?st=searchVacancy&text=NAME%3A%28Data+scien*+OR+Data+analys*+OR+Python+OR+%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85+OR+%D0%9F%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82*+%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+OR+Big+Data+OR+Data+Engineer+OR+Product+Analys*+OR+ML+engineer+OR+Machine+learning+OR+Computer+scien*%29&area=1&area=78&area=2&area=53&area=237&area=88&area=99&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=&items_on_page=100&no_magic=true&L_save_area=true
-# https://samara.hh.ru/search/vacancy?st=searchVacancy&text=NAME%3A%28Data+scien*+OR+Data+analys*+OR+Python+OR+%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85+OR+%D0%9F%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82*+%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+OR+Big+Data+OR+Data+Engineer+OR+Product+Analys*+OR+ML+engineer+OR+Machine+learning+OR+Computer+scien*%29&area=1&area=78&area=2&area=53&area=237&area=88&area=99
-# https://samara.hh.ru/search/vacancy?st=searchVacancy&text=NAME%3A%28Data+scien*+OR+Data+analys*+OR+Python+OR+%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85+OR+%D0%9F%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82*+%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+OR+Big+Data+OR+Data+Engineer+OR+Product+Analys*+OR+ML+engineer+OR+Machine+learning+OR+Computer+scien*%29&area=1
-# search_query = 'NAME:(Data scien* OR Data analys* OR Python OR Аналитик данных OR Продукт* аналитик OR Big Data OR Data Engineer OR Product Analys* OR ML engineer OR Machine learning OR Computer scien*)'
-
+vac_name = input('Введите наименование вакансии для поиска: ')
 main_link = 'https://samara.hh.ru'
-# params = {'st': 'searchVacancy',
-#           'text': 'NAME%3A%28Data+scien*+OR+Data+analys*+OR+Python+OR+%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85+OR+%D0%9F%D1%80%D0%BE%D0%B4%D1%83%D0%BA%D1%82*+%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA+OR+Big+Data+OR+Data+Engineer+OR+Product+Analys*+OR+ML+engineer+OR+Machine+learning+OR+Computer+scien*%29',
-#           'area': '1'}
-params = {'st': 'searchVacancy', 'text': 'Data scientist', 'area': '1', 'order_by': 'publication_time', 'only_with_salary': 'false', 'items_on_page': '100'}
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-    'Accept': '*/*'}
+params = {'st': 'searchVacancy', 'text': vac_name, 'search_field': 'name', 'area': '1', 'only_with_salary': 'false',
+          'order_by': 'publication_time', 'items_on_page': '100'}
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/83.0.4103.97 Safari/537.36', 'Accept': '*/*'}
 
 response = requests.get(main_link + '/search/vacancy', params=params, headers=headers)
 
-# with open('vac_list.json', 'w') as f:
-#     json.dump(response.json(), f)
-#
-# # data = response.json()
-# data = open('vac_list.json', 'r')
-# sleep(10)
 soup = bs(response.text, 'lxml')
-
-# with open('vac_list.txt', 'w') as f:
-#     # f.write(soup.lxml)
-#     json.dump(soup.json(), f)
 
 vacancies_block = soup.find('div', {'class': 'vacancy-serp'})
 
 vacancies_list = vacancies_block.findChildren(recursive=False)
-# next_word = soup.find('a', {'class': 'bloko-button HH-Pager-Controls-Next HH-Pager-Control'}).text
-# print(next_word)
-# print(vacancies_list)
+
 vacancies = []
-# tag_link = soup.find('a', {'class': 'bloko-link HH-LinkModifier'})
-# link = tag_link['href']
-# print(link)
 num_steps = 0
-# num_steps = int(vacancies_list.find('a', {'data-qa': 'pager-next'}).text)
+
 while True:
     for vacancy in vacancies_list:
-
         vacancy_data = {}
 
         try:
             tag_link = vacancy.find('a', {'class': 'bloko-link HH-LinkModifier'})
-            # print(tag_link)
             link = tag_link['href']
             name = tag_link.text
-            # print(name)
 
             tag_link_c = vacancy.find('a', {'class': 'bloko-link bloko-link_secondary'})
             link_c = main_link + tag_link_c['href']
@@ -89,7 +63,6 @@ while True:
                 city_zone1 = None
 
             try:
-                # salary = vacancy.find('span', {'class': 'bloko-section-header-3 bloko-section-header-3_lite'}).nextSibling.text
                 salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
             except AttributeError:
                 salary = None
@@ -118,7 +91,7 @@ while True:
             vacancy_data['company'] = company
             vacancy_data['company_link'] = link_c
             vacancy_data['city'] = city
-            vacancy_data['city_zone1'] = city_zone1
+            # vacancy_data['city_zone1'] = city_zone1   починить, дает лишние данные 'Показать на карте'
             vacancy_data['salary_min'] = salary_min
             vacancy_data['salary_max'] = salary_max
             vacancy_data['salary_curr'] = salary_curr
@@ -129,7 +102,7 @@ while True:
             continue
     try:
         if soup.find('a', {'class': 'bloko-button HH-Pager-Controls-Next HH-Pager-Control'}).text == 'дальше':
-            next_link = soup.find('a', {'class': 'bloko-link bloko-link_secondary'})['href']
+            next_link = soup.find('a', {'class': 'bloko-button HH-Pager-Controls-Next HH-Pager-Control'})['href']
             response = requests.get(main_link + next_link, headers=headers)
             soup = bs(response.text, 'lxml')
             vacancies_block = soup.find('div', {'class': 'vacancy-serp'})
@@ -142,5 +115,9 @@ while True:
             break
     except AttributeError:
         break
+
 pprint(vacancies)
-print(f'Найдено {num_steps} вакансий')
+print(f'Найдено {num_steps} вакансий на сайте {main_link}')
+
+with open('vacancies.json', 'w') as f:
+    f.write(str(vacancies))
